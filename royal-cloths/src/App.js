2a05ee87ba5a9ca41b065e9be642af0a80abc200
sahanpasindu <sahanpasindu07@gmail.com>
2020-09-25 12:17:48 +0530

    Creating our first component, homepage and adding scss to help us with writing our styles

--- royal-cloths/src/App.js
+++ royal-cloths/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <HomePage />
     </div>
   );
 }
