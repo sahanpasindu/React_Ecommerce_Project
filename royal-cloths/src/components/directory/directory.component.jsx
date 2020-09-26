492959f83195badd3e3f5790d993a012759aba46
sahanpasindu <sahanpasindu07@gmail.com>
2020-09-26 10:17:06 +0530

    Adding routing with react-router, then creating new routes for our existing homepage and our new hats page

--- royal-cloths/src/components/directory/directory.component.jsx
+++ royal-cloths/src/components/directory/directory.component.jsx
@@ -15,29 +15,34 @@ class Directory extends React.Component {
             {
                title: 'hats',
                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-               id: 1
+               id: 1,
+               linkUrl: 'hats',
             },
             {
                title: 'jackets',
                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-               id: 2
+               id: 2,
+               linkUrl: '',
             },
             {
                title: 'sneakers',
                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-               id: 3
+               id: 3,
+               linkUrl: '',
             },
             {
                title: 'womens',
                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                size: 'large',
-               id: 4
+               id: 4,
+               linkUrl: '',
             },
             {
                title: 'mens',
                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                size: 'large',
-               id: 5
+               id: 5,
+               linkUrl: '',
             }
          ]
       };
@@ -47,8 +52,8 @@ class Directory extends React.Component {
       return (
          <div className="directory-menu">
             {
-               this.state.sections.map(({ id, title, imageUrl, size }) => (
-                  <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+               this.state.sections.map(({ id, ...otherSectionProps }) => (
+                  <MenuItem key={id} {...otherSectionProps} />
                ))
             }
          </div>
