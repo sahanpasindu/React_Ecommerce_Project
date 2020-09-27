2beafc0a3e1d84b5388848df312afaf75b448e06
sahanpasindu <sahanpasindu07@gmail.com>
2020-09-27 14:30:40 +0530

    Creating header component to demonstrate where react-router renders our content on the page using <route> component

--- royal-cloths/src/App.js
+++ royal-cloths/src/App.js
@@ -4,15 +4,19 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
-    <Switch>
-      <div>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </div>
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <div>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </div>
+      </Switch>
+    </div>
   );
 }
 
