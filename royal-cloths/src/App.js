cce8e2d137c61410468cf50b40755337b9df1ccd
sahanpasindu <sahanpasindu07@gmail.com>
2020-09-29 13:01:22 +0530

    Storing our Google authenticated user data in firestore database.

--- royal-cloths/src/App.js
+++ royal-cloths/src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -22,10 +22,10 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      var add = { age: 20, school: 'abc' };
+      createUserProfileDocument(user, add);
+    });
   }
 
   componentWillUnmount() {
--- royal-cloths/src/firebase/firebase.utils.js
+++ royal-cloths/src/firebase/firebase.utils.js
@@ -22,4 +22,28 @@ const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+   if (!userAuth) return;
+
+   console.log(userAuth);
+
+   const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+   const snapShot = await userRef.get();
+
+   if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createAt = new Date();
+
+      try {
+         await userRef.set({
+            displayName, email, createAt, ...additionalData
+         });
+      } catch (error) {
+         console.log('error creating user', error.message);
+      }
+   }
+   return userRef;
+}
+
 export default firebase;
