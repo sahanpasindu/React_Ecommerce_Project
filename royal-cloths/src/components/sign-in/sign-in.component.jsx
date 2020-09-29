42f8614d4491c1270fc58b588f9dc715f071df98
sahanpasindu <sahanpasindu07@gmail.com>
2020-09-29 07:42:53 +0530

    Add google sign out and header link change according to authentication state in our application

--- royal-cloths/src/components/sign-in/sign-in.component.jsx
+++ royal-cloths/src/components/sign-in/sign-in.component.jsx
@@ -52,7 +52,7 @@ class SignIn extends React.Component {
 
                <div className="buttons">
                   <CustomButton type="submit" value="Submit"> Sign in</CustomButton>
-                  <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google</CustomButton>
+                  <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google</CustomButton>
                </div>
             </form>
          </div>
