344338c4bb650aa743f26deffeee2ef5c1065ac2
sahanpasindu <sahanpasindu07@gmail.com>
2020-10-05 12:21:53 +0530

    Now that we have created all the redux code for storing our data (group same item and update the quantity), now we are going to connect our items to add them to our cart.

--- royal-cloths/src/redux/cart/cart.reducer.js
+++ royal-cloths/src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import { CartActionType } from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
    hidden: true,
@@ -15,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       case CartActionType.ADD_ITEM:
          return {
             ...state,
-            cartItems: [...state.cartItems, action.payload]
+            cartItems: addItemToCart(state.cartItems, action.payload)
          }
       default: return state
    }
