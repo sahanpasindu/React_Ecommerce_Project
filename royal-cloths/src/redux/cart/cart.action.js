93a18bf412131eac9e67ffc8921ba881cb053e89
sahanpasindu <sahanpasindu07@gmail.com>
2020-10-05 11:37:40 +0530

    We have updated our cart redux files to handle adding an item, we have also connected it to our updated collection-item.

--- royal-cloths/src/redux/cart/cart.action.js
+++ royal-cloths/src/redux/cart/cart.action.js
@@ -3,3 +3,8 @@ import { CartActionType } from './cart.types';
 export const toggleCartHidden = () => ({
    type: CartActionType.TOGGLE_CART_HIDDEN,
 });
+
+export const addItem = item => ({
+   type: CartActionType.ADD_ITEM,
+   payload: item
+});
