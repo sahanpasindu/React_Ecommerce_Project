81f889c6ff8e05ff9c9f960636722898f5decc07
sahanpasindu <sahanpasindu07@gmail.com>
2020-10-09 21:39:59 +0530

    We have created a action and reducer to remove a cart item from the cart, also we bindded our action with remove button click.

--- royal-cloths/src/redux/cart/cart.action.js
+++ royal-cloths/src/redux/cart/cart.action.js
@@ -8,3 +8,8 @@ export const addItem = item => ({
    type: CartActionType.ADD_ITEM,
    payload: item
 });
+
+export const clearItemFromCart = item =>({
+   type: CartActionType.CLEAR_ITEM_FROM_CART,
+   payload: item
+})
