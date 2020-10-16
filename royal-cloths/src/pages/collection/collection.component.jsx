736a13fc7cb6f90f72b9c0121d7c6c6fc7dd925e
sahanpasindu <sahanpasindu07@gmail.com>
2020-10-16 12:13:06 +0530

    Added nested routing for shop collection page.

---
+++ royal-cloths/src/pages/collection/collection.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection }) => {
+   console.log(collection);
+   return (
+      <div className="collection-page">
+         <h2>CATEGORY PAGE</h2>
+      </div>
+   );
+}
+
+const mapStateToProps = (state, ownProps) => ({
+   collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
