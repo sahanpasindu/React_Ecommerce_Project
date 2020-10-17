1162112c5c8df6adb8d03f99f697b7beaef86a6d
sahanpasindu <sahanpasindu07@gmail.com>
2020-10-17 11:13:06 +0530

    We made change our select Collection selector and convert shop data array to an object to fast quering.

--- royal-cloths/src/pages/collection/collection.component.jsx
+++ royal-cloths/src/pages/collection/collection.component.jsx
@@ -8,10 +8,17 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
-   console.log(collection);
+   const { title, items } = collection;
    return (
       <div className="collection-page">
-         <h2>CATEGORY PAGE</h2>
+         <h2 className='title'>{ title }</h2>
+         <div className="items">
+            {
+               items.map(item => (
+                  <CollectionItem key={ item.id } item={ item } />
+               ))
+            }
+         </div>
       </div>
    );
 }
